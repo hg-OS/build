7bc1076a7be662a93e66128c1e419c884b89ea0f
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-05-11 20:35:05 +0000

    build: Add our props to ADDITIONAL_BUILD_PROPERTIES
    Change-Id: I81f55f3a0a91e79eca4dcd22cf8f10354fff7b30
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -154,6 +154,10 @@ else
   endif
 endif
 
+#
+# -----------------------------------------------------------------
+# Add our styx properties to the build properties.
+ADDITIONAL_BUILD_PROPERTIES += $(STYX_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk

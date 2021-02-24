StatiXOS/android_build

build: properly call roomservice
Change-Id: I64dc9eea55e1e244abbec4a57c3a33ac01edbd52

envsetup.sh
@@ -661,7 +661,7 @@ function lunch()
         if [[ $NO_ROOMSERVICE == true ]]; then
             echo "Roomservice turned off, type in 'export NO_ROOMSERVICE=false' if you want it back on"
         else
-            vendor/statix/build/tools/roomservice.py $product
+            python3 vendor/statix/build/tools/roomservice.py $product
         fi
         popd > /dev/null
         check_product $product

hg-OS/build

envsetup.sh
@@ -661,7 +661,7 @@ function lunch()
         if [[ $NO_ROOMSERVICE == true ]]; then
             echo "Roomservice turned off, type in 'export NO_ROOMSERVICE=false' if you want it back on"
         else
-            vendor/styx/build/tools/roomservice.py $product
+            python3 vendor/styx/build/tools/roomservice.py $product
         fi
         popd > /dev/null
         check_product $product